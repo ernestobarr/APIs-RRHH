b6cac0f55f164ace5edabdb32e71e07f8b616fe4
Ernestobarr <95388097+ernestobarr@users.noreply.github.com>
2023-11-04 17:43:35 -0600

    update

---
+++ src/test/java/com/parcial03/p3/P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial03.p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
